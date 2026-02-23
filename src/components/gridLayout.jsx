244e18d597fee2279a5ab9901c421dcf2ba82d37
pini weiss <pini6775@gmail.com>
2026-02-23 11:28:21 +0200

    start logic

--- src/components/gridLayout.jsx
+++ src/components/gridLayout.jsx
@@ -6,14 +6,17 @@ function GridLayout({ incement }) {
   function press(e) {
     if (!e.target.className.includes("square-revealed")) {
       incement();
-      e.target.className = " square-revealed";
+      e.target.className = "square-revealed";
     }
+    if(e.target.className.includes("secret-color")){e.target.className = "secret-color-expolre"}
   }
+  gridArray[10] = "secret-color"
 
   return (
     <div id="grid-board">
-      {gridArray.map((_, index) => (
-        <div onClick={press} key={index} className="square"></div>
+      {gridArray.map((square, index) => (
+        
+        <div onClick={press} key={index} className={square === "secret-color"? "secret-color square": "square"} ></div>
       ))}
     </div>
   );
