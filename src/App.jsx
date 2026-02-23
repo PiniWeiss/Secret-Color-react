9c21947332a109be3bc051b4e82623f7c36eabb2
pini weiss <pini6775@gmail.com>
2026-02-23 09:15:39 +0200

    header initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import GridLayout from "./components/gridLayout";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <main>
+        <Header/>
         <GridLayout />
       </main>
     </>
