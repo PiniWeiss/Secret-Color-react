fdb5d6894c48d2b7b9f31c499ad32e0f49f51a8f
pini weiss <pini6775@gmail.com>
2026-02-23 09:25:42 +0200

    header design done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-
-
 function Header() {
   return (
     <header>
-      <h1>Find the <span className='title-color'>Secret Color!</span><span className='colorto-find'></span></h1>
+      <h1>
+        Find the<span className="title-color">Secret Color!</span>
+        <div className="color-to-find"></div>
+      </h1>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
